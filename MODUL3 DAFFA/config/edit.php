50a9b26db120931002aad158c1e383a9e819cea4
alfianmf <alfianm999@gmail.com>
2022-11-25 08:32:47 +0700

    debug

--- MODUL3 DAFFA/config/edit.php
+++ MODUL3 DAFFA/config/edit.php
@@ -1,45 +1,45 @@
-<?php 
-    require './connector.php';
+        <?php 
+            require './connector.php';
 
-    if(isset($_POST['edit_car'])) {
-        // delete existed photo
-        $photo_path = "../asset/images/".$foto;
-        unlink($photo_path . $query_all);
+            if(isset($_POST['edit_car'])) {
+                // delete existed photo
+                $photo_path = "../asset/images/".$foto;
+                unlink($photo_path . $query_all);
 
-        //This is the directory where images will be saved 
-        $target = "../asset/images/"; 
-        $target = $target . basename( $_FILES['photo']['name']);
+                //This is the directory where images will be saved 
+                $target = "../asset/images/"; 
+                $target = $target . basename( $_FILES['photo']['name']);
 
-        // Writes the photo to the server
-        if(move_uploaded_file($_FILES['photo']['tmp_name'], $target)){
+                // Writes the photo to the server
+                if(move_uploaded_file($_FILES['photo']['tmp_name'], $target)){
 
-        }
+                }
 
-        // edit data for update
-        $nama_mobil = $_POST["nama"];
-        $nama_pemilik = $_POST["pemilik"];
-        $merk = $_POST["merk"];
-        $tanggal_beli = $_POST["tanggal_beli"];
-        $deskripsi = $_POST["deskripsi"];
-        $status_mobil = $_POST["status"];
+                // edit data for update
+                $nama_mobil = $_POST["nama"];
+                $nama_pemilik = $_POST["pemilik"];
+                $merk = $_POST["merk"];
+                $tanggal_beli = $_POST["tanggal_beli"];
+                $deskripsi = $_POST["deskripsi"];
+                $status_mobil = $_POST["status"];
 
-        // get all file
-        $get_all_data = "SELECT * FROM showroom_daffa_table ORDER BY id_mobil";
-        $get_all_run = mysqli_query($connection, $get_all_data);
+                // get all file
+                $get_all_data = "SELECT * FROM showroom_daffa_table ORDER BY id_mobil";
+                $get_all_run = mysqli_query($connection, $get_all_data);
 
-        // make gloabl variable for data
-        global $foto;
+                // make gloabl variable for data
+                global $foto;
 
-        // membuat array data
-        while($row = mysqli_fetch_array($get_all_run)) {
-            $foto = $row["foto_mobil"];
-        }
+                // membuat array data
+                while($row = mysqli_fetch_array($get_all_run)) {
+                    $foto = $row["foto_mobil"];
+                }
 
-        // query for update
-        $query_syntax = "UPDATE showroom_daffa_table SET nama_mobil='$nama_mobil', pemilik_mobil='$nama_pemilik', merk_mobil='$merk', tanggal_beli='$tanggal_beli', deskripsi='$deskripsi', status_pembayaran='$status_mobil'";
-        $edit_query = mysqli_query($connection, $query_syntax);
+                // query for update
+                $query_syntax = "UPDATE showroom_daffa_table nama_mobil='$nama_mobil', pemilik_mobil='$nama_pemilik', merk_mobil='$merk', tanggal_beli='$tanggal_beli', deskripsi='$deskripsi', status_pembayaran='$status_mobil'";
+                $edit_query = mysqli_query($connection, $query_syntax);
 
-        // redirect to edit page
-        header("Location:/TUGAS_PRAKTIKUM_WAD/MODUL3%20DAFFA/pages/Edit-DAFFA.php");
-    }
-?>
+                // redirect to edit page
+                header("Location:/TUGAS_PRAKTIKUM_WAD/MODUL3%20DAFFA/pages/Edit-DAFFA.php");
+            }
+        ?>
